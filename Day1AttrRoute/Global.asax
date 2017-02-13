c48f480c14a2651d6ea92d0b7c3557ed7c3535e1
taddeimania <jtaddei@gmail.com>
2017-02-13 09:51:22 -0500

    Add project files.

---
+++ Day1AttrRoute/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Day1AttrRoute.MvcApplication" Language="C#" %>
---
+++ Day1AttrRoute/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Day1AttrRoute.Startup))]
+namespace Day1AttrRoute
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
